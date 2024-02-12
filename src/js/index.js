e1e7d2889e4c9d9bc29ba4d7bec5270b35812324
Parviz <Parvi3@yandex.ru>
2024-02-12 23:29:00 +0300

    fix-3

--- src/js/index.js
+++ src/js/index.js
@@ -1,119 +1,118 @@
-import('../scss/index.scss');
-import('../js/_feedback.js');
-import('../js/_asideLeft.js');
-import('../js/_articleTextShow.js');
-import('../js/_requestСall.js');
+import("../js/_feedback.js");
+import("../js/_asideLeft.js");
+import("../js/_articleTextShow.js");
+import("../js/_requestСall.js");
 
 let swiper;
 let init = false;
 
-const mobile = window.matchMedia('(min-width: 0) and (max-width: 767px)');
-const tablet = window.matchMedia('(min-width: 768px) and (max-width: 1439px)');
-const desktop = window.matchMedia('(min-width: 1440px)');
+const mobile = window.matchMedia("(min-width: 0) and (max-width: 767px)");
+const tablet = window.matchMedia("(min-width: 768px) and (max-width: 1439px)");
+const desktop = window.matchMedia("(min-width: 1440px)");
 const brandsTablet = document.querySelectorAll(
-    '.brands__list div:nth-last-child(-n + 5)',
+  ".brands__list div:nth-last-child(-n + 5)",
 );
 const brandsDesktop = document.querySelectorAll(
-    '.brands__list div:nth-last-child(-n + 3)',
+  ".brands__list div:nth-last-child(-n + 3)",
 );
 const techTablet = document.querySelectorAll(
-    '.technology__list div:nth-last-child(-n + 2)',
+  ".technology__list div:nth-last-child(-n + 2)",
 );
 const techDesktop = document.querySelectorAll(
-    '.technology__list div:nth-last-child(-n + 1)',
+  ".technology__list div:nth-last-child(-n + 1)",
 );
-const btnHide = document.querySelector('.btn__hide');
-const btnShow = document.querySelector('.btn__show');
-const btnShow2 = document.querySelector('.btn__show_2');
-const btnHide2 = document.querySelector('.btn__hide_2');
+const btnHide = document.querySelector(".btn__hide");
+const btnShow = document.querySelector(".btn__show");
+const btnShow2 = document.querySelector(".btn__show_2");
+const btnHide2 = document.querySelector(".btn__hide_2");
 
 // eslint-disable-next-line require-jsdoc
 function swiperMod() {
   if (mobile.matches) {
     if (!init) {
       init = true;
-      swiper = new Swiper('.swiper', {
-        slidesPerView: 'auto',
+      swiper = new Swiper(".swiper", {
+        slidesPerView: "auto",
         loop: true,
         spaceBetween: 16,
         pagination: {
-          el: '.swiper-pagination',
+          el: ".swiper-pagination",
           clickable: true,
         },
       });
-      btnHide.style.display = 'none';
+      btnHide.style.display = "none";
       // btnShow.style.display = "none";
     }
   } else if (tablet.matches || desktop.matches) {
     swiper.destroy();
     init = false;
-    btnHide.style.display = 'none';
-    btnShow.style.display = 'block';
+    btnHide.style.display = "none";
+    btnShow.style.display = "block";
   }
 }
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   swiperMod();
 });
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   swiperMod();
 });
 
-btnShow.addEventListener('click', () => {
+btnShow.addEventListener("click", () => {
   if (tablet.matches) {
     brandsTablet.forEach((button) => {
-      button.style.display = 'flex';
+      button.style.display = "flex";
     });
   }
   if (desktop.matches) {
     brandsDesktop.forEach((button) => {
-      button.style.display = 'flex';
+      button.style.display = "flex";
     });
   }
-  btnHide.style.display = 'block';
-  btnShow.style.display = 'none';
+  btnHide.style.display = "block";
+  btnShow.style.display = "none";
 });
-btnHide.addEventListener('click', () => {
+btnHide.addEventListener("click", () => {
   if (tablet.matches) {
     brandsTablet.forEach((button) => {
-      button.style.display = 'none';
+      button.style.display = "none";
     });
   }
   if (desktop.matches) {
     brandsDesktop.forEach((button) => {
-      button.style.display = 'none';
+      button.style.display = "none";
     });
   }
-  btnShow.style.display = 'block';
-  btnHide.style.display = 'none';
+  btnShow.style.display = "block";
+  btnHide.style.display = "none";
 });
 
-btnShow2.addEventListener('click', () => {
+btnShow2.addEventListener("click", () => {
   if (tablet.matches) {
     techTablet.forEach((button) => {
-      button.style.display = 'grid';
+      button.style.display = "grid";
     });
   }
   if (desktop.matches) {
     techDesktop.forEach((button) => {
-      button.style.display = 'grid';
+      button.style.display = "grid";
     });
   }
-  btnHide2.style.display = 'block';
-  btnShow2.style.display = 'none';
+  btnHide2.style.display = "block";
+  btnShow2.style.display = "none";
 });
-btnHide2.addEventListener('click', () => {
+btnHide2.addEventListener("click", () => {
   if (tablet.matches) {
     techTablet.forEach((button) => {
-      button.style.display = 'none';
+      button.style.display = "none";
     });
   }
   if (desktop.matches) {
     techDesktop.forEach((button) => {
-      button.style.display = 'none';
+      button.style.display = "none";
     });
   }
-  btnShow2.style.display = 'block';
-  btnHide2.style.display = 'none';
+  btnShow2.style.display = "block";
+  btnHide2.style.display = "none";
 });
